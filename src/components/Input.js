1656249bc31cd30de03afe3f5c8ecf9182d92920
devr11 <rastogi.db1@gamil.com>
2024-11-01 20:10:20 +0530

    Add new features

--- src/components/Input.js
+++ src/components/Input.js
@@ -14,6 +14,13 @@ const Input = React.forwardRef( function Input({
                 htmlFor={id}
                 </label>
                 }
+                <input
+                type={type}
+                className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+                ref={ref}
+                {...props}
+                id={id}
+                />
         </div>
     )
 })
